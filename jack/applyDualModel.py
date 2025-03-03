bf0e7f30e46ee77685e97562c697eae66e2e2bad
Jack Cheng <jcheng50@stuy.edu>
2025-03-02 23:06:12 -0500

    conference weighting done on agg 1 and agg 2

--- jack/applyDualModel.py
+++ jack/applyDualModel.py
@@ -10,8 +10,12 @@ import colleyDivisions
 import colleyTeams
 
 teams = pd.read_csv('../data/updateRegionGroups.csv')
+teamr = pd.read_csv('three_rankings.csv')
+#print(teamr)
 
-teamRankings = colleyTeams.returnTeamRank()
+teamRankings1 = teamr[['team', 'agg_rating_1']].values.tolist()
+teamRankings2 = teamr[['team', 'agg_rating_2']].values.tolist()
+teamRankingsColley = teamr[['team', 'colley_rating']].values.tolist()
 
 divisionRankings = colleyDivisions.returnDivisionRank()
 
@@ -22,14 +26,14 @@ for count, team in enumerate(usefulTeams):
     teamConference[team] = associatedConference[count]
 
 print("-----PRE CONFERENCE RANKINGS------")
-for rank in teamRankings:
+for rank in teamRankings2:
     print(rank)
-for ranking in teamRankings:
+for ranking in teamRankings2:
     conference = teamConference[ranking[0]]
     ranking[1] = float(ranking[1] * divisionRankings[conference])
 
 print("-----------------------------------")
 print("-----POST CONFERENCE RANKINGS------")
-sort = sorted(teamRankings, key=lambda x: x[1], reverse=True)
+sort = sorted(teamRankings2, key=lambda x: x[1], reverse=True)
 for rank in sort:
     print(rank)
